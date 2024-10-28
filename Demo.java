67daa48a71a85925f88dffeb2fe2c77ca87c9d27
kalaprabhakar <137256829+kalaprabhakar@users.noreply.github.com>
2024-10-28 15:16:58 +0530

    update

--- Demo.java
+++ Demo.java
@@ -2,6 +2,6 @@ class Demo
 {
 	public static void main(String[] args) 
 	{
-		system.out.println("This is the second test code.");
+		System.out.println("This is the second test code.");
 	}
 }
--- Demo.java.bak
+++ Demo.java.bak
@@ -2,6 +2,6 @@ class Demo
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("This is the second test code.");
+		system.out.println("This is the second test code.");
 	}
 }
